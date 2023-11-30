022720901f53fd93e118ec2da181597e7aff0c89
Tejas Bandbe <tejasbandbe65@gmail.com>
2023-11-30 08:50:42 +0530

    emoji functionality complete

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -5,18 +5,21 @@ import axios from "axios";
 import { allUsersRoute } from '../utils/APIRoutes';
 import Contacts from '../components/Contacts';
 import Welcome from '../components/Welcome';
+import ChatContainer from '../components/ChatContainer';
 
 function Chat() {
   const history = useHistory();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     if(!localStorage.getItem("chat-app-user")){
       history.push("/login");
     }else{
       setCurrentUser(JSON.parse(localStorage.getItem("chat-app-user")));
+      setIsLoaded(true);
     }
   },[]);
 
@@ -44,10 +47,15 @@ function Chat() {
       <div className="container">
         <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange}/>
 
-        <Welcome currentUser={currentUser}/>
+      {
+        isLoaded && currentChat === undefined ?
+        (<Welcome currentUser={currentUser}/>) :
+        (<ChatContainer currentChat={currentChat}/>)
+      }
+
       </div>
     </Container>
-  )
+)
 }
 
 const Container = styled.div`
